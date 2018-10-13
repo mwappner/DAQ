dc25b24b15fac1c5067ecce81731a0110d25bc53
0xInfty <anirix.vall@gmail.com>
2018-10-13 15:07:14 -0300

    Archivo 'script'. Cambios: 1 Funcionales (-) + 3 Sintácticos (+).
    Cambios sintácticos (+):
    	modifico nombre de variables como 't' ó 'med' a 'time' ó 'signal' para que sean más legibles.
    
    Cambios sintácticos (-):
    	agrego documentación;
    	agrego comentarios.
    
    Cambios funcionales (-):
    	emprolijo formato de gráficos.

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 10 14:55:37 2018
+This script is to make measurements with a National Instruments DAQ.
 
-@author: Publico
+@author: GrupoFWP
 """
 
 import nidaqmx as nid
@@ -12,51 +12,102 @@ import fwp_save as sav
 import os
 import fwp_lab_instruments as ins
 
-def main_freq(data, sr):
-    #Calculates main frequency in signal and its power
+def main_frequency(data, samplerate):
+    """Returns main frequency and its Fourier amplitude.
+    
+    Parameters
+    ----------
+    data : np.array
+        Data inside a 1D array.
+    samplerate : int, float
+        The data's sampling rate.
+    
+    Returns
+    -------
+    samplerate : int, float
+        The data's sampling rate.
+    main_frequency : float
+        The data's main frequency.
+    fourier_peak : float
+        The main frequency's fourier amplitude.
+    """
+    
     fourier = np.abs(np.fft.rfft(data))
-    fourier_freqs = np.fft.rfftfreq(len(data), d=1./sr)
-    max_freq = fourier_freqs[np.argmax(fourier)]
-    pot = max(fourier)
-    return sr, max_freq, pot
+    fourier_frequencies = np.fft.rfftfreq(len(data), d=1./sr)
+    max_frequency = fourier_frequencies[np.argmax(fourier)]
+    fourier_peak = max(fourier)
+    
+    return samplerate, max_frequency, fourier_peak
 
-#%% Juan
+#%% Only_One_Measure
+"""
+This script makes a voltage measurement.
 
+It measures at differential mode. Then, it applies Fourier 
+Transformation to get the main frequency and its Fourier intensity. It 
+only plots the data.
+
+Warnings
+--------
+Taken from 'github.com/DopplerPavlovTichno/Placa-adquisicion' f35f2c2
+Subtly modified by FWP Group
+"""
+
+# PARAMETERS
+
+# Main parameters
 duration = 1
 samplerate = 5e3
 
+# Other parameters
 samples_per_channel = samplerate * duration
-t = np.arange(0, duration, 1/samplerate)
-#med = np.nan
-mode = nid.constants.TerminalConfiguration.NRSE # Sets terminal configuration
+time = np.arange(0, duration, 1/samplerate)
+mode = nid.constants.TerminalConfiguration.NRSE # Terminal configuration
 # DIFFERENTIAL, NRSE, RSE, PSEUDODIFFERENTIAL
-t = np.arange(0, duration, 1/samplerate)
 
+# ACTIVE CODE
+
+# Make a measurement using NI DAQ.
 with nid.Task() as task:
     task.ai_channels.add_ai_voltage_chan(
-            "Dev20/ai1",
+            "Dev20/ai1", # analog input AI 1
             terminal_config=mode)
-    task.timing.cfg_samp_clk_timing(samplerate,
-                                    samps_per_chan=int(samples_per_channel))
-    med = task.read(number_of_samples_per_channel=int(samples_per_channel))
+    task.timing.cfg_samp_clk_timing(
+            samplerate,
+            samps_per_chan=int(samples_per_channel))
+    data = task.read(
+            number_of_samples_per_channel=int(samples_per_channel))
     task.wait_until_done()
 
-plt.plot(med)
+# Make Fourier transformation and get main frequency
+fourier = np.abs(np.fft.rfft(data)) # Fourier transformation
+fourier_frequencies = np.fft.rfftfreq(len(data), d=1./samplerate)
+max_frequency = fourier_frequencies[np.argmax(fourier)]
 
-fourier = np.abs(np.fft.rfft(med))
-fourier_freqs = np.fft.rfftfreq(len(med), d=1./samplerate)
-frecuencia = fourier_freqs[np.argmax(fourier)]
-plt.plot(t, med)
-plt.xlabel('tiempo (s)')
-plt.ylabel('tension (V)')
+# Plot data
 plt.figure()
-plt.plot(fourier_freqs, fourier)
-plt.xlabel('frecuencia (Hz)')
-plt.ylabel('abs(fourier) (ua)')
-plt.title('{}'.format(frecuencia))
+plt.plot(time, data)
+plt.xlabel('Tiempo (s)')
+plt.ylabel('Tensión (V)')
+
+# Plot Fourier
+plt.figure()
+plt.plot(fourier_frequencies, fourier)
+plt.xlabel('Frecuencia (Hz)')
+plt.ylabel('Intensidad de Fourier (ua)')
+plt.title('{}'.format(max_frequency))
 
 #%% Samplerate_Sweep
+"""This scripts makes a sampling rate sweep for a fixed signal.
 
+This code saves all measurements. It also applies Fourier Transformation 
+to them, to get the main frequency and its Fourier amplitude. Then, it 
+saves main frequency and its Fourier amplitude along with samplig rate.
+"""
+
+# PARAMETERS
+
+# Main parameters
 periods_to_meassure = 100
 signal_frequency = 1.2e3
 
@@ -67,25 +118,26 @@ mode = nid.constants.TerminalConfiguration.NRSE
 
 name = 'Samplerate_Sweep'
 
+# Other parameters
 samplerate = np.linspace(samplerate_min,
                          samplerate_max,
                          samplerate_n)
-
-
 duration = periods_to_meassure / signal_frequency
+
 folder = os.path.join(os.getcwd(),
                       'Measurements',
                       name)
 folder = sav.new_dir(folder)
-filename = lambda samplerate : os.path.join(folder, 
-                                            '{:.0f}_Hz.txt'.format(samplerate))
-
+filename = lambda samplerate : os.path.join(
+        folder, 
+        '{:.0f}_Hz.txt'.format(samplerate))
 header = 'time [s]  data [V]'
 
+# ACTUAL CODE
+               
 fourier_data = [] # contains sampling rate, main frequency and its power
-
-
 with nid.Task() as task:
+    
     task.ai_channels.add_ai_voltage_chan(
             "Dev20/ai1",
             terminal_config=mode)
@@ -94,15 +146,19 @@ with nid.Task() as task:
         print('Doing {}Hz'.format(sr))
         
         samples_to_meassure = int(sr * duration)
-        task.timing.cfg_samp_clk_timing(rate=sr,
-                                        samps_per_chan=samples_to_meassure)
-        med = task.read(number_of_samples_per_channel=samples_to_meassure)
+        task.timing.cfg_samp_clk_timing(
+                rate=sr,
+                samps_per_chan=samples_to_meassure)
+        
+        signal = task.read(
+                number_of_samples_per_channel=samples_to_meassure)
         task.wait_until_done()
-#        len(med )
-        t = np.linspace(0, duration, samples_to_meassure)
-
-        np.savetxt(filename(sr), np.array([t,med]).T, header=header)
-        fourier_data.append(main_freq(med, sr))
+        
+        time = np.linspace(0, duration, samples_to_meassure)
+        np.savetxt(filename(sr), np.array([time, signal]).T, 
+                   header=header)
+        
+        fourier_data.append(main_frequency(signal, sr))
 
 sav.savetext(np.array(fourier_data), os.path.join(folder, 'Data.txt'), 
              header=['Sampling rate (Hz)', 
@@ -110,29 +166,33 @@ sav.savetext(np.array(fourier_data), os.path.join(folder, 'Data.txt'),
                      'Intensity'])
 
 #%% Frequency_Sweep
+"""This script makes a double sweep on frequency and samplerate.
+
+For each frequency, it sweeps on samplerate. It saves all measurements. 
+It also applies Fourier Transformation to get main frequency and its 
+Fourier amplitude. For each frequency, it saves main frequency and 
+Fourier amplitude along with samplerate.
+"""
 
 # PARAMETERS
 
-signal_frequency = 1.2e3
-
+# Main parameters
 samplerate_min = 10
 samplerate_max = 400e3
 samplerate_n = 5#100
 mode = nid.constants.TerminalConfiguration.NRSE
 
 signal_frequency_n = 10#500
+port = 'ASRL1::INSTR'
 
 name = 'Frequency_Sweep'
 
-port = 'ASRL1::INSTR'
-
-# OTHER PARAMETERS
-
-gen = ins.Gen(port=port, nchannels=2)
-
+# Other parameters
 samplerate = np.linspace(samplerate_min,
                          samplerate_max,
                          samplerate_n)
+
+gen = ins.Gen(port=port, nchannels=2)
 
 folder = os.path.join(os.getcwd(),
                       'Measurements',
@@ -140,16 +200,18 @@ folder = os.path.join(os.getcwd(),
 folder = sav.new_dir(folder)
 filename = lambda samplerate, signal_frequency : os.path.join(
         folder, 
-        'Sr_{:.0f}_Hz_Freq_{:.0f}_Hz.txt'.format(signal_frequency, samplerate))
+        'Sr_{:.0f}_Hz_Freq_{:.0f}_Hz.txt'.format(signal_frequency, 
+                                                 samplerate))
 filename_fourier = lambda samplerate : os.path.join(
         folder,
         'Sr_{:.0f}_Hz_Fourier.txt'.format(samplerate))
-header = 'time [s]\tdata [V]'
-header_fourier = 'Sampling rate (Hz)\tMain frequency (Hz)\tIntensity'
+header = ['Time [s]', 'Data [V]']
+header_fourier = ['Sampling rate (Hz)',
+                  'Main frequency (Hz)',
+                  'Intensity (u.a.)']
 
+# ACTIVE CODE
 fourier_data = [] # contains sampling rate, main frequency and its power
-
-
 with nid.Task() as task:
     
     task.ai_channels.add_ai_voltage_chan(
@@ -157,8 +219,7 @@ with nid.Task() as task:
             terminal_config=mode)
         
     for sr in samplerate:
-        
-        print('Doing SR {}Hz'.format(sr))
+        print('Doing SR {} Hz'.format(sr))
         
         signal_frequency_min = sr/10
         signal_frequency_max = 5*sr
@@ -167,34 +228,37 @@ with nid.Task() as task:
                                        signal_frequency_n)
         
         duration = 10/signal_frequency_min
-
         samples_to_meassure = int(sr * duration)
-        task.timing.cfg_samp_clk_timing(rate=sr,
-                                        samps_per_chan=samples_to_meassure)
+        task.timing.cfg_samp_clk_timing(
+                rate=sr,
+                samps_per_chan=samples_to_meassure)
         
         for freq in signal_frequency:
             
             gen.output(True, channel=1, print_changes=False,
                        frequency=freq, amplitude=2)
 
-            med = task.read(number_of_samples_per_channel=samples_to_meassure)
+            signal = task.read(
+                    number_of_samples_per_channel=samples_to_meassure)
             task.wait_until_done()
 
-            t = np.linspace(0, duration, samples_to_meassure)
-    
-            np.savetxt(filename(sr), np.array([t,med]).T, header=header)
-            fourier_data.append(main_freq(med, sr))
+            time = np.linspace(0, duration, samples_to_meassure)
+            np.savetxt(filename(sr), np.array([time, signal]).T, 
+                       header=header)
+            
+            fourier_data.append(main_frequency(signal, sr))
 
-        np.savetext(np.array(fourier_data), 
-                    os.path.join(folder, 
-                                 filename_fourier), 
-                    header=header_fourier)
+        sav.savetext(np.array(fourier_data), 
+                     os.path.join(folder, 
+                                  filename_fourier), 
+                     header=header_fourier)
 gen.gen.close()
 
 #%% Moni_Freq
 
 with nid.Task() as task:
-    task.ai_channels.add_ai_freq_voltage_chan("Dev20/ai1",max_val=20000)
+    task.ai_channels.add_ai_freq_voltage_chan("Dev20/ai1", 
+                                              max_val=20000)
     med = task.read()
     task.wait_until_done()
 
