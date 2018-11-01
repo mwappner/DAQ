d92ff288720adccf486b7d21a00802fcc83ec2aa
0xInfty <anirix.vall@gmail.com>
2018-11-01 19:51:03 -0300

    Módulo 'fwp_daq'. Cambios: 2 Funcionales (+).
    Cambios funcionales (+):
    	cambio de enfoque y utilizo 'properties' para 'AnalogInputs'.
    
    Cambios funcionales (-):
    	en vez de que los atributos sean listas dinámicas, defino el 'property.setter' para regresar listas dinámicas.

--- fwp_daq.py
+++ fwp_daq.py
@@ -44,6 +44,8 @@ def devices():
 
 def dynamic_list(inlist):
     
+    if inlist is None:
+        return(None)
     if len(inlist) == 0:
         raise TypeError('Empty list!')
     if len(inlist) == 1:
@@ -59,28 +61,43 @@ class Task:
         
         self.__device = device
         self.__task = nid.Task()
-        self._analog_inputs = None
-        self._pwm_outputs = None
+        self.__analog_inputs = None
+        self.__pwm_outputs = None
     
     @property
     def analog_inputs(self):
-        return self.analog_inputs
+        return self.__analog_inputs
     
     @analog_inputs.setter
     def analog_inputs(self, pins):
         
-        self.analog_inputs = AnalogInputs(
-                pins=pins,
-                device=self.__device,
-                task=self.__task,
-                )
+        if self.__analog_inputs is None:
+            self.__analog_inputs = AnalogInputs(
+                    pins=pins,
+                    device=self.__device,
+                    task=self.__task,
+                    )
+        else:
+            voltage_range = self.__analog_inputs.range
+            configuration = self.__analog_inputs.configuration
+            self.__analog_inputs = AnalogInputs(
+                    pins,
+                    voltage_range=voltage_range,
+                    configuration=configuration,
+                    device=self.__device,
+                    task=self.__task,
+                    )
+        
+    @property
+    def pwm_outputs(self):
+        return self.__pwm_outputs
 
     
 #%%
 
 class AnalogInputs:
 
-    def init(self, pins=None, device=None, task=None, 
+    def init(self, pins, device=None, task=None, 
              voltage_range=[-10, 10], 
              configuration=conf.NRSE):
 
@@ -93,9 +110,9 @@ class AnalogInputs:
         pins : int, list
             Device's pins to initialize as analog input channel/s.
         voltage_range=[-10,10] : list
-            Range of the analog input channel/s. Each of them should be a 
-            list or tuple that contains minimum and maximum in V.
-        configuration=NRSE : nid.constants.TerminalConfiguration, optional
+            Range of the analog input channel/s. Each of them should be 
+            a list or tuple that contains minimum and maximum in V.
+        configuration=NRSE : nid.constants, optional
             Analog input channel/s terminal configuration.
         
         Returns
@@ -107,41 +124,138 @@ class AnalogInputs:
         
         self.__device = device
         self.__task = task
-        self.__pins = pins
+        self.pins = pins
+        # This attribute sets also...
+        # ...self.channels,
+        # ...self.__ai_channels, 
+        # ...self.nchannels
         
-        if pins is None:
-            raise ValueError("Can't initialize AnalogInputs without pins")
-        
-        self.channels = self.__channels__(pins)
-        self.nchannels = len(self.channels)
-    
-        if not isinstance(voltage_range, list):
-            voltage_range = [voltage_range for ch in self.channels]
-        elif len(voltage_range) != self.nchannels:
-            raise ValueError("Not enough elements in voltage_range")
-        self.range = voltage_range
-        
-        if not isinstance(configuration, list):
-            configuration = [configuration for ch in self.channels]
-        elif len(configuration) != self.nchannels:
-            raise ValueError("Not enough elements in voltage_range")
         self.configuration = configuration
+        self.input_range = voltage_range
+    
+    @property
+    def pins(self):
+        return dynamic_list(self.__pins)
+    
+    @pins.setter
+    def pins(self, pins):
+        
+        self.__pins = pins
+        self.__channels = self.__channels__(pins)
+        if pins is None:
+            self.__nchannels = 0
+        else:
+            self.__nchannels = len(self.channels)
         
         ai_channels = []
         for i, ch in enumerate(self.channels):
             ai_channels.append(
                 self.__task.ai_channels.add_ai_voltage_chan(
                     physical_channel = ch,
-                    min_val = voltage_range[i][0],
-                    max_val = voltage_range[i][1],
                     units = nid.constants.VoltageUnits.VOLTS
                     )
                 )
-            ai_channels[i].ai_term_cfg = configuration[i]
+        self.__ai_channels = ai_channels
     
-        self.__channels = dynamic_list(ai_channels)
+    @property
+    def channels(self):
+        return dynamic_list(self.__channels)
+    
+    @channels.setter
+    def channels(self, channels):
+        message = "Can't be manually set!"
+        message = message + "Must set AnalogInputs.pins"
+        raise AttributeError(message)
+
+    @property
+    def nchannels(self):
+        return self.__nchannels
+    
+    @nchannels.setter
+    def nchannels(self, channels):
+        message = "Can't be manually set!"
+        message = message + "Must set AnalogInputs.pins"
+        raise AttributeError(message)
+    
+    @property
+    def configuration(self):
+        return dynamic_list(self.__configuration)
+    
+    @configuration.setter
+    def configuration(self, mode):
         
-        return dynamic_list(ai_channels)    
+        if not isinstance(mode, list):
+            mode = [mode for ch in self.channels]
+        elif len(mode) != self.nchannels:
+            raise ValueError("Not enough elements in configuration")
+        
+        if self.nchannels == 1:
+            self.__channels.ai_term_cfg = mode[0]
+        else:
+            for i, m in enumerate(mode):
+                if self.__configuration[i] != m:
+                    self.__channels[i].ai_term_cfg = m
+        self.__configuration = mode
+    
+    @property
+    def input_range(self):
+        return self.__range
+    
+    @input_range.setter
+    def input_range(self, voltage_range):
+        
+        if not isinstance(voltage_range, list):
+            voltage_range = [voltage_range]
+        if len(voltage_range) != self.nchannels:
+            raise ValueError("Not enough elements in input_range")
+        for vr in voltage_range:
+            if len(vr) != 2:
+                print("{} should be tuple or list of lenght 2")
+        
+        for i, v in enumerate(voltage_range):
+            if self.__range[i] != v:
+                if self.__range[i][0] != v[0]:
+                    self.__ai_channels[i].ai_min = v[0]
+                    self.__input_min[i] = v[0]
+                if self.__range[i][1] != v[1]:
+                    self.__ai_channels[i].ai_max = v[1]
+                    self.__input_max[i] = v[1]
+        self.__configuration = voltage_range
+        
+    @property
+    def input_min(self):
+        return self.__input_min
+    
+    @input_min.setter
+    def input_min(self, voltage):
+        
+        if not isinstance(voltage, list):
+            voltage = [voltage]
+        if len(voltage) != self.nchannels:
+            raise ValueError("Not enough elements in input_min")
+        
+        for i in range(self.nchannels):
+            if self.__input_min[i] != voltage[i]:
+                self.__ai_channels[i].ai_min = voltage[i]
+        self.__input_min = voltage
+                
+    @property
+    def input_max(self):
+        return self.__input_max
+    
+    @input_max.setter
+    def input_max(self, voltage):
+        
+        if not isinstance(voltage, list):
+            voltage = [voltage]
+        if len(voltage) != self.nchannels:
+            raise ValueError("Not enough elements in input_min")
+        
+        for i in range(self.nchannels):
+            if self.__input_max[i] != voltage[i]:
+                self.__ai_channels[i].ai_max = voltage[i]
+        self.__input_max = voltage
+    
     
     def __channels__(self, pins):
         
@@ -150,14 +264,17 @@ class AnalogInputs:
         Parameters
         ----------
         pins : int, list
-            Pins to be initialized. Each of them should be 
-            an int from 0 to 15.
+            Pins to be initialized. Each of them should be an int from 
+            0 to 15.
         
         Returns
         -------
         channels : str, list
             Channels
         """
+        
+        if pins is None:
+            return None
         
         reference = [15, 17, 19, 21, 24, 26, 29, 31, 
                      16, 18, 20, 22, 25, 27, 30, 32]
@@ -172,7 +289,7 @@ class AnalogInputs:
                 raise ValueError(message.format(p))
             channels.append(p)
         
-        return dynamic_list(channels)
+        return channels
 
 #%%
 
