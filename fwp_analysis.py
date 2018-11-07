facf3878c5bc5f316181ba593a0e705857e3513e
mwappner <42619775+mwappner@users.noreply.github.com>
2018-11-07 15:33:10 -0300

    Agrego clase para PID controller

--- fwp_analysis.py
+++ fwp_analysis.py
@@ -704,3 +704,51 @@ def peak_separation(signal, time=1, *args, **kwargs):
         peak_times = peaks * time
         
     return np.mean(peak_times)
+
+#%% PID class
+    
+class PIDController:
+    ''' A simple class implementing a PID contoller.
+    
+    Pseudocode example:
+        
+    lazo = PIDController(42, 3, 2, 1)
+    
+    while True:
+
+    signal = read()
+
+    actuator = lazo.calculate(signal)
+
+    write(actuator)
+    '''
+
+    def __init__(self, setpoint, kp=1.0, ki=0.0, kd=0.0, dt=1):
+
+        self.setpoint = setpoint
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.dt = dt
+
+        self.last_error = 0
+        self.p_term = 0
+        self.i_term = 0
+        self.d_term = 0
+
+    def calculate(self, feedback_value):
+        error = self.setpoint - feedback_value
+
+        delta_error = error - self.last_error
+
+        self.p_term = error
+        self.i_term += error * self.dt
+        self.d_term = delta_error / self.dt
+
+        self.last_error = error
+
+        return (self.kp * self.p_term) + (self.ki * self.i_term) + (self.kd * self.d_term)
+
+
+
+    
