41f0e5b5bc12fa3cd7b0931db29895994a30321c
0xInfty <anirix.vall@gmail.com>
2018-11-12 02:08:05 -0300

    Script 'SLoop'. Cambios: 3 Funcionales (+) + 3 Sintácticos (+).
    	Agrego sección 'Read_N_Write' para probar la clase 'Task' con algo sencillo.
    	Agrego sección 'Read_N_Write_Callback' para probar un callback sencillo y comprenderlo.
    	Agrego sección 'Control_Loop_2' que usa 'PIDControllerWithLog' y guarda el log al final.
    
    Cambios funcionales (+):
    	defino fuera del 'with' del 'open' una función 'callback_definer' que toma 'file' y regresa 'callback', de forma que dentro del 'with' se asigne 'callback' en 1 línea;
    	comento la sección que lee dentro del callback y asumo que 'callback_data' contiene datos leídos.
    
    Cambios funcionales (-):
    	agrego reconfiguración de terminal de canales analógicos.
    
    Cambios sintácticos (-):
    	agrego comentarios.
    
    Cambios sintácticos (+):
    	cambio 'pwm_default_duty_cycle' a 'pwm_initial_duty_cycle' que ya no es una lista sino un valor pequeño;
    	cambio de nombre parámetros dentro de la sección 'Now I apply PID' de 'Control_Loop'.

--- SLoop.py
+++ SLoop.py
@@ -12,12 +12,14 @@ control loop that can raise an object at a constant given velocity.
 import fwp_analysis as fan
 import fwp_daq as daq
 import fwp_daq_channels as fch
+import fwp_plot as fwplot
+import fwp_save as sav
 import matplotlib.pyplot as plt
-import nidaqmx as nid
+#import nidaqmx as nid
 import numpy as np
 import os
 from time import sleep
-conf = nid.constants.TerminalConfiguration
+#conf = nid.constants.TerminalConfiguration
 
 #%% PWM_Single
 
@@ -102,9 +104,13 @@ nsamples = int(200e3)
 
 # Initialize communication
 task = daq.Task(device, mode='r')
+"""Could be initialized on test mode:
+task = daq.Task('Dev1', mode='r', conection=False)
+"""
         
 # Configure input
 task.add_channels(fch.AnalogInputChannel, *ai_pins)
+task.all.configuration = ai_conf
 
 # Make a measurement
 signal = task.read(nsamples_total=10000, 
@@ -140,6 +146,7 @@ task = daq.Task(device, mode='r')
         
 # Configure input
 task.add_channels(fch.AnalogInputChannel, *ai_pins)
+task.all.configuration = ai_conf
 
 # Make a continuous measurement
 signal = task.read(nsamples_total=None, # None means continuous
@@ -154,6 +161,130 @@ task.close()
 plt.figure()
 plt.plot(signal)
 
+#%% Read_N_Write
+
+"""Reads the PWM signal it writes"""
+
+# PARAMETERS
+
+device = daq.devices()[0]
+
+ai_pin = 15 # Literally the number of the DAQ pin
+ai_conf = 'Ref' # Referenced mode (measure against GND)
+
+pwm_pin = 38 # Literally the number of the DAQ pin
+pwm_frequency = 100e3
+pwm_duty_cycle = np.linspace(.1,1,10)
+
+nsamples = 10000
+samplerate = 100e3
+nsamples_each = 1000
+
+# ACTIVE CODE
+
+# Initialize communication
+task = daq.DAQ(device)
+
+# Configure input channel
+task.add_analog_inputs(ai_pin)
+task.inputs.ai0.configuration = ai_conf
+task.inputs.samplerate = samplerate
+
+# Configure output channel
+task.add_pwm_outputs(pwm_pin)
+
+# Measure
+task.outputs.write(status=True) # Output on
+signal = task.inputs.read(nsamples_total=nsamples,
+                          samplerate=samplerate,
+                          nsamples_each=nsamples_each)
+task.outputs.write(status=False) # Output off
+
+# End communication                  
+task.close()
+
+# Get time
+samplerate = task.inputs.samplerate
+time = np.arange(0, len(signal)/samplerate, 1/samplerate)
+
+# Plot
+fig = plt.figure()
+plt.xlabel('Time (s)')
+plt.ylabel('Data (V)')
+fwplot.add_style()
+plt.plot(time, signal, '.')
+
+#%% Read_N_Write_Callback
+
+"""Allows to try and understand callback"""
+
+# PARAMETERS
+
+device = daq.devices()[0]
+
+ai_pin = 15 # Literally the number of the DAQ pin
+ai_conf = 'Ref' # Referenced mode (measure against GND)
+
+pwm_pin = 38 # Literally the number of the DAQ pin
+
+nsamples = 10000
+samplerate = 100e3
+nsamples_each = 1000
+nsamples_callback = 2000
+
+# ACTIVE CODE
+
+# Initialize communication
+task = daq.DAQ(device)
+
+# Configure input channel
+task.add_analog_inputs(ai_pin)
+task.inputs.ai0.configuration = ai_conf
+task.inputs.samplerate = samplerate
+
+# Configure output channel
+task.add_pwm_outputs(pwm_pin)
+
+# Define callback
+data = []
+samples = []
+event = []
+def callback(task_handle, every_n_samples_event_type,
+             number_of_samples, callback_data):
+
+    """A really simple callback that only appends data to lists"""
+    
+    data.append(callback_data)
+    samples.append(number_of_samples)
+    event.append(every_n_samples_event_type)
+    
+    return 0
+
+# Measure
+task.outputs.write(status=True) # Output on
+task.inputs.read(nsamples_total=nsamples,
+                 samplerate=samplerate,
+                 nsamples_each=nsamples_each,
+                 callback=callback,
+                 nsamples_callback=nsamples_callback)
+task.outputs.write(status=False)
+task.close()
+
+# Plot
+print("List 'callback_data' has {} elements".format(len(data)))
+for i, d in enumerate(data):
+    fig = plt.figure()
+    plt.title('Series {:.0f}'.format(i))
+    plt.xlabel('Time (s)')
+    plt.ylabel('Callback data (V)')
+
+# See other data
+print("List 'number_of_samples' has {} elements".format(len(samples)))
+print(samples)
+print("List 'every_n_samples_event_type' has {} elements".format(
+        len(samples)))
+print(event)
+
 #%% Control_Loop
 
 # PARAMETERS
@@ -165,7 +296,7 @@ ai_conf = 'Ref' # Referenced mode (measure against GND)
 
 pwm_pin = 38 # Literally the number of the DAQ pin
 pwm_frequency = 100e3
-pwm_default_duty_cycle = np.linspace(.1,1,10)
+pwm_initial_duty_cycle = 0.1
 
 wheel_radius = 0.025 # in meters
 
@@ -183,50 +314,152 @@ task = daq.DAQ(device)
 
 # Configure inputs
 task.add_analog_inputs(ai_pin)
-#task.inputs.configuration = ai_conf
-task.inputs.ai0._AnalogInputChannel__channel.ai_term_cfg = conf.NRSE
+task.inputs.configuration = ai_conf
+#task.inputs.ai0._AnalogInputChannel__channel.ai_term_cfg = conf.NRSE
 
 # Configure outputs
 task.add_pwm_outputs(pwm_pin)
 task.outputs.frequency = pwm_frequency
-task.outputs.duty_cycle = pwm_default_duty_cycle
+task.outputs.duty_cycle = pwm_initial_duty_cycle
 
-# Control loop's saving mechanism
-with open(os.path.join('Measurements','log.txt'), 'w') as file:
-    file.write('Vel\t duty_cycle\t P\t I\t D') # First line
+# Define a callback definer
+def callback_definer(file):
     
-    # Control loop's callback
     def callback(task_handle, every_n_samples_event_type,
                  number_of_samples, callback_data):
-            
-        # First I read a few values
-        samples = task.read(nsamples_total=nsamples_callback,
-                            samplerate=samplingrate)
+
+#        # First I read a few values        
+#        samples = task.read(nsamples_total=nsamples_callback,
+#                            samplerate=samplingrate)
         
         # Now I apply PID
-        d = np.diff(samples)
-        vel = fan.peak_separation(d, pid.dt, prominence=1, height=2)
-        vel = vel * wheel_radius
-        new_dc = pid.calculate(vel)
+#        photogate_derivative = np.diff(samples)
+        photogate_derivative = np.diff(callback_data)
+        angular_velocity = fan.peak_separation(photogate_derivative, 
+                                               pid.dt, prominence=1, 
+                                               height=2)
+        velocity = angular_velocity * wheel_radius
+        new_dc = pid.calculate(velocity)
         
         # Then I save some data
-        data = fan.append_data_to_string(vel, new_dc, 
+        data = fan.append_data_to_string(velocity, new_dc, 
                                          pid.p_term, pid.i_term, 
                                          pid.d_term)
+        """Wouldn't it be better to save old_dc, velocity?"""
         file.write(data)
         
         # And finally I change duty cycle
         task.ouputs.duty_cycle = fan.set_bewtween(new_dc, *(0,100))
     
-    # Turn on outputs
-    task.outputs.status = True
+        return 0
     
-    # Start measurement
+    return callback
+
+# Start measuring
+with open(os.path.join('Measurements','log.txt'), 'w') as file:
+    file.write('Vel\t duty_cycle\t P\t I\t D') # First line
+
+    # Define callback
+    callback = callback_definer(file)
+    
+    # Measure
+    task.outputs.status = True
     signal = task.inputs.read(nsamples_total=None,
                               nsamples_each=500,
                               samplerate=samplingrate,
                               nsamples_callback=nsamples_callback,
                               callback=callback)
-    
     task.outputs.status = False
+    
+    # Close communication
     task.close()
+
+#%% Control_Loop_2
+
+# PARAMETERS
+
+device = daq.devices()[0]
+
+ai_pin = 15 # Literally the number of the DAQ pin
+ai_conf = 'Ref' # Referenced mode (measure against GND)
+
+pwm_pin = 38 # Literally the number of the DAQ pin
+pwm_frequency = 100e3
+pwm_duty_cycle = np.linspace(.1,1,10)
+
+wheel_radius = 0.025 # in meters
+
+nsamples_callback = 20
+samplingrate = 100e3
+nsamples_each = 1000
+
+#pid = fan.PIDController(setpoint=1, kp=10, ki=5, kd=7, 
+#                        dt=1/samplingrate)
+pid = fan.PIDControllerWithLog(setpoint=1, kp=10, ki=5, kd=7, 
+                               dt=1/samplingrate)
+
+# ACTIVE CODE
+
+# Initialize communication for writing and reading at the same time
+task = daq.DAQ(device)
+
+# Configure inputs
+task.add_analog_inputs(ai_pin)
+task.inputs.configuration = ai_conf
+
+# Configure outputs
+task.add_pwm_outputs(pwm_pin)
+task.outputs.frequency = pwm_frequency
+task.outputs.duty_cycle = pwm_duty_cycle[0]
+
+# Define callback
+#log = []
+def callback(task_handle, every_n_samples_event_type,
+                 number_of_samples, callback_data):
+    
+    # Now I apply PID
+    photogate_derivative = np.diff(callback_data)
+    angular_velocity = fan.peak_separation(photogate_derivative, 
+                                           pid.dt, prominence=1, 
+                                           height=2)
+    velocity = angular_velocity * wheel_radius
+    new_dc = pid.calculate(velocity)
+    
+#    # Then I save some data
+#    log.append([velocity, 
+#                new_dc, 
+#                pid.p_term, 
+#                pid.i_term, 
+#                pid.d_term])
+    
+    # And finally I change duty cycle
+    task.ouputs.duty_cycle = fan.set_bewtween(new_dc, *(0,100))
+
+    return 0
+  
+# Measure
+task.outputs.status = True
+signal = task.inputs.read(nsamples_total=None,
+                          nsamples_each=500,
+                          samplerate=samplingrate,
+                          nsamples_callback=nsamples_callback,
+                          callback=callback)
+task.outputs.status = False
+
+# Close communication
+task.close()
+
+# Save log
+sav.savetxt(os.path.join(os.getcwd(), 'Measurements', 'Log.txt'),
+            np.array(pid.log),
+            header=['Feedback value (m/s)',
+                    'New value (a.u.)',
+                    'Proportional term (u.a.)',
+                    'Integral term (u.a.)',
+                    'Derivative term (u.a.)'],
+            footer=dict(ai_conf=ai_conf,
+                        pwm_frequency=pwm_frequency,
+                        pid=str(pid.__class__),
+                        samplerate=samplingrate,
+                        nsamples_each=nsamples_each,
+                        nsamples_callback=nsamples_callback))
