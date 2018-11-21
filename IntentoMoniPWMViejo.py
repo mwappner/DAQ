37650ccafefa3694be2b90b517aad08d7c7dc90f
Monica Far <mfar@puesto3.cuartos.inv.dc.uba.ar>
2018-11-21 12:24:24 -0300

    Corrijo unas cosas (tabulaciones, reordenar, faltaba una linea de código)

--- IntentoMoniPWMViejo.py
+++ IntentoMoniPWMViejo.py
@@ -1,19 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 20 19:50:49 2018
 
-@author: mfar
-"""
-# -*- coding: utf-8 -*-
-"""
-This script is to make measurements with a National Instruments DAQ.
-
-This script is based on our old script 'SOldLoop'. It works using the 
-'fwp_daq' module we designed. This script's goal goal is to make a 
-control loop that can raise an object at a constant given velocity.
-
-@author: GrupoFWP
-"""
 
 import queue
 import threading
@@ -32,9 +18,6 @@ from nidaqmx import stream_writers as sw
 import nidaqmx.stream_readers as sread
 import nidaqmx as nid
 import daq
-
-
-
 
 
 
@@ -62,48 +45,6 @@ pid = fan.PIDController(setpoint=1, kp=10, ki=5, kd=7,
                         dt=nsamples_each/samplingrate, 
                         log_data=True)
 
-# ACTIVE CODE
-
-# Initialize communication for writing and reading at the same time
-with nid.Task() as read_task, nid.Task() as task_co:
-
-    # Channels configuration
-    read_task.ai_channels.add_ai_voltage_chan(ai_pin,
-        max_val=10, min_val=-10)
-
-    # Measurement configuration
-    reader = sread.AnalogMultiChannelReader(read_task.in_stream)
-    values_read = np.zeros((1, nsamples_each), 
-                  dtype=np.float64)
-
-    # Configure clock output
-    channels_co = daq.pwm_output_channels(
-            task_co,
-            physical_channels = pwm_channels,
-            frequency = pwm_frequency,
-            duty_cycle = pwm_duty_cycle[0]
-            )
-   
-    # Set contiuous PWM signal
-    task_co.timing.cfg_implicit_timing(
-            sample_mode = nid.constants.AcquisitionType.CONTINUOUS)
-   
-    # Create a PWM stream
-    stream_co = sw.CounterWriter(task_co.out_stream)
-
-    # Play   
-    task_co.start()    
-     
-    # Make measurement
-    read_task.start()
-    
-
-    #np.testing.assert_allclose(values_read, rtol=0.05, atol=0.005)
-
-#%%
-
-pid.reset()
-pid.clearlog()
 
 # Define callback's replacement
 def calculate_duty(read_data):
@@ -144,13 +85,66 @@ def worker():
                 duty_cycle = channels_co.co_pulse_duty_cyc
                 )
         print(3)
-        
-t = threading.Thread(target=worker)
-t.start()
+            
 
-while True:
-    data = values_read
-    q.put(np.array(data))
-    sleep(.1)
+
+
+
+
+
+
+
+# Initialize communication for writing and reading at the same time
+with nid.Task() as read_task, nid.Task() as task_co:
+
+    # Channels configuration
+    read_task.ai_channels.add_ai_voltage_chan(ai_pin,
+        max_val=10, min_val=-10)
+
+    # Measurement configuration
+    reader = sread.AnalogMultiChannelReader(read_task.in_stream)
+    values_read = np.zeros((1, nsamples_each), 
+                  dtype=np.float64)
+
+    # Configure clock output
+    channels_co = daq.configure_pwm(
+            task_co,
+            physical_channels = pwm_channels,
+            frequency = pwm_frequency,
+            duty_cycle = pwm_duty_cycle[0]
+            )
+   
+    # Set contiuous PWM signal
+    task_co.timing.cfg_implicit_timing(
+            sample_mode = nid.constants.AcquisitionType.CONTINUOUS)
+   
+    # Create a PWM stream
+    stream_co = sw.CounterWriter(task_co.out_stream)
+
+    # Play   
+    task_co.start()    
+     
+    # Make measurement
+    read_task.start()
+    
+
+    #np.testing.assert_allclose(values_read, rtol=0.05, atol=0.005)
+    t = threading.Thread(target=worker)
+    t.start()
+#%%
+    
+    pid.reset()
+    pid.clearlog()
+    
+
+    
+    while True:
+        reader.read_many_sample(
+            values_read, 
+            number_of_samples_per_channel=nsamples_each,
+            timeout=2)
+        data = values_read
+        q.put(np.array(data))
+        sleep(.1)
     
     
